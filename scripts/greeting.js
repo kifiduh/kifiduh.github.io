4888ed4a16e8f7fd7c0bf9a1a9ec653b3ede60d9
kifiduh <thediamondsniper2098@gmail.com>
2026-03-22 09:54:20 -0500

    add cool site greeting

---
+++ scripts/greeting.js
@@ -0,0 +1,12 @@
+const mainParagraph = document.getElementById("main-paragraph");
+        
+const now = new Date();
+const hours = now.getHours();
+        
+if (hours <= 12) {
+    mainParagraph.innerHTML = "<i>good morning, welcome to my website! <br> click one of the images below to navigate through the site! </i>"
+} else if (hours >= 12 || hours < 19) {
+    mainParagraph.innerHTML = "<i>good evening, welcome to my website! <br> click one of the images below to navigate through the site! </i>"
+} else if (hours >= 19) {
+    mainParagraph.innerHTML = "<i>good night, welcome to my website! <br> click one of the images below to navigate through the site! </i>"
+}
