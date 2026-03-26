bb7fcc4dbc7e8c93108869b8f5c1d68f645424be
kifiduh <thediamondsniper2098@gmail.com>
2026-03-26 02:03:25 -0500

    birthday script recode

--- scripts/birthday.js
+++ scripts/birthday.js
@@ -1,56 +1,30 @@
 const bdayLabel = document.getElementById("bday");
         
 function updateBirthday() {
-    function timeSince(date) {
+
+    function timeSince(pastDateString) {
+        const pastDate = new Date(pastDateString);
         const now = new Date();
-                
-        const years = now.getYear() - date.getYear()
 
-        let months = 0
-        let days = 0
-        let hours = 0
-        let minutes = 0
+        const differenceInMs = now.getTime() - pastDate.getTime();
 
-        if (now.getMonth() < date.getMonth) {
-            months = date.getMonth() - now.getMonth()
-        } else if (now.getMonth() >= date.getMonth()) {
-            months = now.getMonth() - date.getMonth()
-        }
+        const msInSecond = 1000;
+        const msInMinute = msInSecond * 60;
+        const msInHour = msInMinute * 60;
+        const msInDay = msInHour * 24;
 
-        if (now.getDate() < date.getDate) {
-            days = date.getDate() - now.getDate()
-        } else if (now.getDate() >= date.getDate()) {
-            days = now.getDate() - date.getDate()
-        }
+        const days = Math.floor(differenceInMs / msInDay);
+        const hours = Math.floor((differenceInMs % msInDay) / msInHour);
+        const minutes = Math.floor((differenceInMs % msInHour) / msInMinute);
+        const seconds = Math.floor((differenceInMs % msInMinute) / msInSecond);
 
-        if (now.getHours() < date.getHours) {
-            hours = date.getHours() - now.getHours()
-        } else if (now.getHours() >= date.getHours()) {
-            hours = now.getHours() - date.getHours()
-        }
-
-        if (now.getMinutes() < date.getMinutes) {
-            minutes = date.getMinutes() - now.getMinutes()
-        } else if (now.getMinutes() >= date.getMinutes()) {
-            minutes = now.getMinutes() - date.getMinutes()
-        }
-
-        const seconds = now.getSeconds() - date.getSeconds()
-                
-        return {
-            years: years,
-            months: months,
-            days: days,
-            hours: hours,
-            minutes: minutes,
-            seconds: seconds
-        };
+        return `i am currently ${years} years, ${months} months, ${days} days, ${hours} hours, ${minutes} minutes, and ${seconds} seconds old!!`
     }
                 
     const pastDate = new Date('2008-03-07T03:11:00');
     const elapsed = timeSince(pastDate);
             
-    bdayLabel.innerHTML = `i am currently ${elapsed.years} years, ${elapsed.months} months, ${elapsed.days} days, ${elapsed.hours} hours, ${elapsed.minutes} minutes, and ${elapsed.seconds} seconds old!!`   
+    bdayLabel.innerHTML = elapsed
 }
         
 const interval = setInterval(updateBirthday, 1000)
