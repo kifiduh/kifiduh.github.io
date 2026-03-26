26f0c39965ef64f1747c928d508a1bbfaaea55aa
kifiduh <thediamondsniper2098@gmail.com>
2026-03-25 21:49:25 -0500

    fix greeting script

--- scripts/greeting.js
+++ scripts/greeting.js
@@ -5,7 +5,7 @@ const hours = now.getHours();
         
 if (hours < 12) {
     mainParagraph.innerHTML = "<i>good morning, welcome to my website! <br> click one of the images above to navigate through the site! </i>"
-} else if (hours >= 12 || hours < 19) {
+} else if (hours >= 12 && hours < 19) {
     mainParagraph.innerHTML = "<i>good evening, welcome to my website! <br> click one of the images above to navigate through the site! </i>"
 } else if (hours >= 19) {
     mainParagraph.innerHTML = "<i>good night, welcome to my website! <br> click one of the images above to navigate through the site! </i>"
