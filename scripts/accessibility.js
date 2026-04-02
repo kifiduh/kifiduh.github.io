175f3650c7f3a36f28419d6877de348d9ba72313
kifiduh <thediamondsniper2098@gmail.com>
2026-04-02 17:15:29 -0500

    i forgot to save data.,

--- scripts/accessibility.js
+++ scripts/accessibility.js
@@ -1,7 +1,9 @@
 input.addEventListener("change", (event) => {
     if (event.target.checked) {
         body.style.fontFamily = "sans-serif"
+        localStorage.setItem("dysMode", "true")
     } else {
         body.style.fontFamily = "starshines"
+        localStorage.setItem("dysMode", "false")
     }
 });
