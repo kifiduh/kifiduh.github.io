a1718dccba729bc9121744604d76397b218e51e0
kifiduh <thediamondsniper2098@gmail.com>
2026-04-02 17:04:41 -0500

    bugfix

--- scripts/accessibility.js
+++ scripts/accessibility.js
@@ -1,7 +1,3 @@
-const body = document.body
-const checkbox = document.querySelector("#checkbox")
-const input = checkbox.querySelector("#dysMode")
-            
 input.addEventListener("change", (event) => {
     if (event.target.checked) {
         body.style.fontFamily = "sans-serif"
