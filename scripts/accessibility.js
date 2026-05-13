47350ee775714abe3f3682da97c46d6f3070880b
kifiduh <thediamondsniper2098@gmail.com>
2026-05-13 18:24:11 -0500

    (hopefully) add dyslexia mode to characters page

--- scripts/accessibility.js
+++ scripts/accessibility.js
@@ -1,3 +1,12 @@
+const checkbox = document.getElementById("checkbox")
+const input = checkbox.querySelector("#dysMode")
+            
+if (dysMode == "true") {
+    input.checked = true
+} else {
+    input.checked = false
+}
+            
 input.addEventListener("change", (event) => {
     if (event.target.checked) {
         body.style.fontFamily = "sans-serif"
